f1d20ee1f0d3385efa3aad5820192ea51a0fe40f
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-09-17 09:22:04 +1000

    build based on focal base image - this can later be switched to neuromachine once working

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,11 +1,11 @@
-docker build -t neurodesk . --file Dockerfile_focal_base && (
+docker build -t neurodesktop_jupyter . --file Dockerfile.focal_base && (
     echo "Starting:"
     @REM sudo docker run -d --privileged --name vnm -v /vnm:/vnm -e RESOLUTION=1600x960 -e USER=neuro -p 6080:80 -p 5900:5900 vnm:latest
-    @REM docker run -d --name neurodesk -p 8888:8888 neurodesk
-    docker run -it --name neurodesk -p 8888:8888 neurodesk
+    @REM docker run -d --name neurodesktop_jupyter -p 8888:8888 neurodesktop_jupyter
+    docker run -it --name neurodesktop_jupyter -p 8888:8888 neurodesktop_jupyter
     set /p=running - press ENTER key to shutdown and quit!
-    docker stop neurodesk
-    docker rm neurodesk
+    docker stop neurodesktop_jupyter
+    docker rm neurodesktop_jupyter
 ) || (
     echo "-------------------------"
     echo "Docker Build failed!"
