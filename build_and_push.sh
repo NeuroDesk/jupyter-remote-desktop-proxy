269eb90171ea634a91d240dc97717ceb4b585d71
aswinnarayanan <aswinn89@gmail.com>
2022-02-09 16:13:49 +1000

    Check for changes in docker image before pushing

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,7 +2,19 @@
 
 timestamp=$(date +%Y%m%d%H%M%S)
 
-docker build . -t vnmd/jupyter-remote-desktop-proxy:$timestamp
-docker push vnmd/jupyter-remote-desktop-proxy:$timestamp
+IMAGEID_NEW="vnmd/jupyter-remote-desktop-proxy:$timestamp"
+IMAGEID_OLD="vnmd/jupyter-remote-desktop-proxy:latest"
 
-echo "FROM vnmd/jupyter-remote-desktop-proxy:$timestamp" > binder/Dockerfile
+docker build . -t $IMAGEID_NEW
+docker build . -t $IMAGEID_OLD -f binder/Dockerfile
+
+ROOTFS_OLD=$(docker inspect --format='{{.RootFS}}' $IMAGEID_OLD)
+ROOTFS_NEW=$(docker inspect --format='{{.RootFS}}' $IMAGEID_NEW)
+
+if [ "$ROOTFS_OLD" != "$ROOTFS_NEW" ]; then
+    echo "Changes found. Pushing new image"
+    docker push $IMAGEID_NEW
+    echo "FROM vnmd/jupyter-remote-desktop-proxy:$timestamp" > binder/Dockerfile
+else
+    echo "No changes found. Skip pushing"
+fi
