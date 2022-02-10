285b0b69d1234fdc036aacb9a58c63ace8445af6
aswinnarayanan <aswinn89@gmail.com>
2022-02-10 13:53:22 +1000

    Run local build in privileged mode

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,4 +3,4 @@
 timestamp=$(date +%Y%m%d%H%M%S)
 
 docker build . -t vnmd/jupyter-remote-desktop-proxy:$timestamp
-docker run -it -p 8888:8888 vnmd/jupyter-remote-desktop-proxy:$timestamp
+docker run --shm-size=1gb -it --privileged --name neurodesktop -p 8888:8888 vnmd/jupyter-remote-desktop-proxy:$timestamp
